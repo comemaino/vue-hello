418f47c9fc20f8314c5253316aef48782bfeb1d7
comemaino <mat.maino@gmail.com>
2022-05-05 14:42:54 +0200

    Init

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data
+
+Vue.config.devtools = true;
+
+const app = new Vue();
